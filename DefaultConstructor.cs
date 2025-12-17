ca94b113604dfa2190105bb031ceb2877559fce0
Bapu <jeebanjyotiparida39@gmail.com>
2025-12-17 18:43:59 +0530

    Added Default Constuctor

---
+++ DefaultConstructor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    internal class DefaultConstructor
+    {
+        public string Name;
+        public DefaultConstructor() { }
+        static void Main(string[] args)
+        {
+            DefaultConstructor instance = new DefaultConstructor();
+            instance.Name = "Bapu";
+            Console.WriteLine("Hello"+instance.Name);
+        }
+    }
+}
